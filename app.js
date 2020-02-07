0cfe2cfb223fe22e5da35f5de7fed9644ce02a13
Versawe <47580432+Versawe@users.noreply.github.com>
2020-02-06 20:35:50 -0500

    2/6
    Have a chat almost up and available. I'm 20 minutes in on the tutorial video with 10 remaining.

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
+users = [];
+connections = [];
+
+serv.listen(2000);
+console.log("Server Started");
 
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/client/Versaw_blackJack.html');
 });
 app.use('/client',express.static(__dirname + '/client'));
 
-serv.listen(2000);
-console.log("Server Started");
 
 var io = require('socket.io')(serv,{});
 io.sockets.on('connection', function(socket){
-	console.log('socket connection');
+	connections.push(socket);
+	console.log('Connected %s sockets connected', connections.length);
+
+	//disconnect
+	socket.on('disconnect', function(data){
+		connections.splice(connections.indexOf(socket),1);
+	console.log('Disconneceted %s sockets connected', connections.length);
+	});
+
+	//send message
+	socket.on('send message', function(data){
+		io.sockets.emit('new message', {msg: data})
+	});
+	
 });
