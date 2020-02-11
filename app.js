9e79982cf534aa65c9cdfb43bf72c5be29f12d0d
Versawe <47580432+Versawe@users.noreply.github.com>
2020-02-10 19:15:03 -0500

    2/10
    Online Chat works with server

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var serv = require('http').Server(app);
 users = [];
 connections = [];
 
-serv.listen(2000);
+serv.listen(process.env.PORT || 2000);
 console.log("Server Started");
 
 app.get('/', function(req, res) {
@@ -20,13 +20,27 @@ io.sockets.on('connection', function(socket){
 
 	//disconnect
 	socket.on('disconnect', function(data){
+		users.splice(users.indexOf(socket.username), 1);
+		updateUsernames();
 		connections.splice(connections.indexOf(socket),1);
-	console.log('Disconneceted %s sockets connected', connections.length);
+		console.log('Disconneceted %s sockets connected', connections.length);
 	});
 
 	//send message
 	socket.on('send message', function(data){
-		io.sockets.emit('new message', {msg: data})
+		io.sockets.emit('new message', {msg: data, user: socket.username})
 	});
-	
+
+	//new user
+	socket.on('new user', function(data, callback){
+		callback(true);
+		socket.username = data;
+		users.push(socket.username);
+		updateUsernames();
+	});
+
+	function updateUsernames(){
+		io.sockets.emit('get users', users);
+	}
+
 });
