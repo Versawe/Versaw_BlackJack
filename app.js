94e8984f2301252e1f4d96588530080b56535a8a
Versawe <47580432+Versawe@users.noreply.github.com>
2020-02-12 17:16:11 -0500

    its a mess and doesn't send data to and from server

--- app.js
+++ app.js
@@ -4,6 +4,26 @@ var serv = require('http').Server(app);
 users = [];
 connections = [];
 
+var variable1 = '';
+var variable2 = 0;
+var variable3 = false;
+var variable4 = false;
+var variable5 = false;
+var variable6 = false;
+var variable7 = true;
+var variable8 = false;
+var variable9 = 0;
+var variable10 = 0;
+var variable11 = true;
+var variable12 = false;
+var variable13 = 100000;
+var variable14 = 0;
+var variable15 = false;
+var variable16 = false;
+var variable17 = false;
+var variable18 = false;
+
+
 serv.listen(process.env.PORT || 2000);
 console.log("Server Started");
 
@@ -37,6 +57,138 @@ io.sockets.on('connection', function(socket){
 		socket.username = data;
 		users.push(socket.username);
 		updateUsernames();
+	});
+
+
+
+	//receiving variable data from client
+	socket.on('update variable 1', function(data){
+			variable1 = data.var1;
+			console.log(variable1);
+	});
+	socket.on('update variable 2', function(data){
+			variable2 = data.var2;
+			console.log(variable2);
+	});
+	socket.on('update variable 3', function(data){
+			variable3 = data.var3;
+			console.log(variable3);
+	});
+	socket.on('update variable 4', function(data){
+			variable4 = data.var4;
+			console.log(variable4);
+	});
+	socket.on('update variable 5', function(data){
+			variable5 = data.var5;
+			console.log(variable5);
+	});
+	socket.on('update variable 6', function(data){
+			variable6 = data.var6;
+			console.log(variable6);
+	});
+	socket.on('update variable 7', function(data){
+			variable7 = data.var7;
+			console.log(variable7);
+	});
+	socket.on('update variable 8', function(data){
+			variable8 = data.var8;
+			console.log(variable8);
+	});
+	socket.on('update variable 9', function(data){
+			variable9 = data.var9;
+			console.log(variable9);
+	});
+	socket.on('update variable 10', function(data){
+			variable10 = data.var10;
+			console.log(variable10);
+	});
+	socket.on('update variable 11', function(data){
+			variable11 = data.var11;
+			console.log(variable11);
+	});
+	socket.on('update variable 12', function(data){
+			variable12 = data.var12;
+			console.log(variable12);
+	});
+	socket.on('update variable 13', function(data){
+			variable13 = data.var13;
+			console.log(variable13);
+	});
+	socket.on('update variable 14', function(data){
+			variable14 = data.var14;
+			console.log(variable14);
+	});
+	socket.on('update variable 15', function(data){
+			variable15 = data.var15;
+			console.log(variable15);
+	});
+	socket.on('update variable 16', function(data){
+			variable16 = data.var16;
+			console.log(variable16);
+	});
+	socket.on('update variable 17', function(data){
+			variable17 = data.var17;
+			console.log(variable17);
+	});
+	socket.on('update variable 18', function(data){
+			variable18 = data.var18;
+			console.log(variable18);
+	});
+
+	//sending variable data to other clients
+	socket.emit('server to client 1',{
+		toclient1: variable1
+	});
+	socket.emit('server to client 2',{
+		toclient2: variable2
+	});
+	socket.emit('server to client 3',{
+		toclient3: variable3
+	});
+	socket.emit('server to client 4',{
+		toclient4: variable4
+	});
+	socket.emit('server to client 5',{
+		toclient5: variable5
+	});
+	socket.emit('server to client 6',{
+		toclient6: variable6
+	});
+	socket.emit('server to client 7',{
+		toclient7: variable7
+	});
+	socket.emit('server to client 8',{
+		toclient8: variable8
+	});
+	socket.emit('server to client 9',{
+		toclient9: variable9
+	});
+	socket.emit('server to client 10',{
+		toclient10: variable10
+	});
+	socket.emit('server to client 11',{
+		toclient11: variable11
+	});
+	socket.emit('server to client 12',{
+		toclient12: variable12
+	});
+	socket.emit('server to client 13',{
+		toclient13: variable13
+	});
+	socket.emit('server to client 14',{
+		toclient14: variable14
+	});
+	socket.emit('server to client 15',{
+		toclient15: variable15
+	});
+	socket.emit('server to client 16',{
+		toclient16: variable16
+	});
+	socket.emit('server to client 17',{
+		toclient17: variable17
+	});
+	socket.emit('server to client 18',{
+		toclient18: variable18
 	});
 
 	function updateUsernames(){
